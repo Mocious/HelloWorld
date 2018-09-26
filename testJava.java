10d916d3a306c86b142230bdde6261278f5cda37
Mocious <Mr.Mocious@users.noreply.github.com>
2018-09-26 10:21:03 +0800

    tdd
    Hello,my friend.This is testing for communication.

---
+++ testJava.java
@@ -0,0 +1,16 @@
+
+
+
+public class testJava{
+	private void SayHello{
+		System.out.println("Hello.world!");
+	}
+}
+
+
+public class Demo{
+	public static void main(String[] args){
+		testJava t=new testJava();
+		t.SayHello();
+	}
+}
